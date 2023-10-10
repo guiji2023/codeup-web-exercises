cc4d7a556b3cbdbca82ccf4a986ce819e002e510
Chao Guan <chaoguan@Chaos-Air.localdomain>
2023-10-10 10:40:36 -0500

    for loop

---
+++ js/for_loops.js
@@ -0,0 +1,23 @@
+
+
+const showMultiplicationTable = (a) =>{
+    for (let i = 1; i < 11; i++){
+        console.log(`${a} x ${i} = ${a * i}`);
+    }
+}
+
+// let num = prompt("Enter a single digit number")
+// num = parseInt(num);
+showMultiplicationTable(7);
+
+
+for (let i = 0; i <11; i++){
+    let num = Math.floor(Math.random() * 180 + 20) ;
+    let typeOfInt = (num % 2 == 1) ? "odd": "even";
+    console.log(`${num} is ${typeOfInt} \n`);
+}
+
+
+for(let i = 100; i > 0; i = i- 5){
+    console.log(i);
+}
