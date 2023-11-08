b6848541f7b58a56d1fd413e7fb3372c16e28150
Chao Guan <chaoguan@Chaos-Air.localdomain>
2023-11-08 11:18:30 -0600

    DOM Events Exercise

---
+++ js/dom-query-js.js
@@ -0,0 +1,21 @@
+const myBTN1 = document.querySelector(".myBTN1");
+const myBTN2 = document.querySelector(".myBTN2");
+const myBTN3 = document.querySelector(".myBTN3");
+const myBTN4 = document.querySelector(".myBTN4");
+const textbox = document.querySelector(".hero-box");
+
+myBTN1.addEventListener("click", e => {
+    textbox.classList.add("fancy-header");
+});
+myBTN2.addEventListener("click", e => {
+    textbox.innerText = "Hello World!";
+});
+myBTN3.addEventListener("click", e => {
+    textbox.innerText = "";
+});
+myBTN4.addEventListener("click", e => {
+    [myBTN1, myBTN2, myBTN3].forEach((i) => {
+        i.classList.add("display-none");
+        textbox.innerText = "You clicked myBTN4, other buttons disappeared";
+    })
+});
