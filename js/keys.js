a279815e5185436dc9ebc87376dba5075541918a
guiji2007 <98149176+guiji2007@users.noreply.github.com>
2023-11-28 09:13:07 -0600

    new key added

--- js/keys.js
+++ js/keys.js
@@ -2,4 +2,5 @@ export const keys = {
   github: "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",
   mapbox:
     "pk.eyJ1IjoiZ3VpamkyMDA3IiwiYSI6ImNscGg2NnJoaTA0N2YyaXBjZHVrY2J3cW0ifQ.bZlkE3kJRGqdSWfjli6omQ",
+  openWeather: "6d45573818a81a7d1321c5e7576dacdf",
 };
